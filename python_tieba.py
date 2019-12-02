20941b3cc27a37c4655cdc05068e8a1ab8ae1ee9
guoxianxue <46958969+guoxianxue@users.noreply.github.com>
2019-12-02 21:13:52 +0800

    Add files via upload

---
+++ python_tieba.py
@@ -0,0 +1,41 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+memory=[]
+form = '''
+    <!DOCTYPE html>
+    <title>郭先学的测试贴吧</title>
+    <form method="post">
+    <textarea name="message"></textarea>
+    <br>
+    <button type="submit">按我发帖子</button>
+    </form>
+    <pre>
+    {}
+    </pre>
+    '''
+
+class Handler(BaseHTTPRequestHandler):
+            def do_POST(self):
+                length = int(self.headers.get('Content-length',0))
+                data = self.rfile.read(length).decode()
+                message = parse_qs(data)["message"][0]
+                message = message.replace("<","&lt;")
+                memory.append(message)
+
+                self.send_response(303)
+                self.send_header('Location','/')
+                self.end_headers()
+
+            def do_GET(self):
+                self.send_response(200)
+                self.send_header('Content-type','text/html; charset=utf-8')
+                self.end_headers()
+
+                mesg=form.format("\n".join(memory))
+                self.wfile.write(mesg.encode())
+
+if __name__ =='__main__':
+            server_address=('',9999)
+            httpd=HTTPServer(server_address,Handler)
+            httpd.serve_forever()
